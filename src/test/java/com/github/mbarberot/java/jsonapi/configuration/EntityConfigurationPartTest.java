891dfe7603d179f4e9cdbef7e18ba1e5634e7fd2
mbarberot <mathieu.barberot@gmail.com>
2016-07-15 14:56:08 +0200

    [equals & hashCode] Testing for EntityConfigurationPart

---
+++ src/test/java/com/github/mbarberot/java/jsonapi/configuration/EntityConfigurationPartTest.java
@@ -0,0 +1,90 @@
+package com.github.mbarberot.java.jsonapi.configuration;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EntityConfigurationPartTest {
+
+    private EntityConfigurationPart x;
+    private EntityConfigurationPart y;
+    private EntityConfigurationPart z;
+
+    @Before
+    public void setUp() throws Exception {
+        x = new EntityConfigurationPart("id");
+        y = new EntityConfigurationPart("id");
+        z = new EntityConfigurationPart("id");
+    }
+
+    @Test
+    public void testEquals_ToSelf() throws Exception {
+        //noinspection EqualsWithItself
+        assertTrue(x.equals(x));
+    }
+
+    @Test
+    public void testEquals_IncompatibleType() throws Exception {
+        //noinspection EqualsBetweenInconvertibleTypes
+        assertFalse(x.equals("foo"));
+    }
+
+    @Test
+    public void testEquals_ToNull() throws Exception {
+        //noinspection ObjectEqualsNull
+        assertFalse(x.equals(null));
+    }
+
+    @Test
+    public void testEquals_isSymmetric() throws Exception {
+        assertTrue(x.equals(y));
+        assertTrue(y.equals(x));
+    }
+
+    @Test
+    public void testEquals_isTransitive() throws Exception {
+        assertTrue(x.equals(y));
+        assertTrue(y.equals(z));
+        assertTrue(x.equals(z));
+    }
+
+    @Test
+    public void testEquals_differentAttributes() throws Exception {
+        assertFalse(x.equals(new EntityConfigurationPart("foo")));
+    }
+
+    @Test
+    public void testEquals_isConsistent() throws Exception {
+        assertTrue(x.equals(y));
+        assertTrue(x.equals(y));
+        assertTrue(x.equals(y));
+
+        EntityConfigurationField notX = new EntityConfigurationField("foo");
+        assertFalse(x.equals(notX));
+        assertFalse(x.equals(notX));
+        assertFalse(x.equals(notX));
+    }
+
+    @Test
+    public void testHashCode_isConsistent() throws Exception {
+        int initialHashCode = x.hashCode();
+        assertEquals(initialHashCode, x.hashCode());
+        assertEquals(initialHashCode, x.hashCode());
+    }
+
+    @Test
+    public void testHashCode_equalsObjects() throws Exception {
+        assertTrue(x.equals(y));
+        assertEquals(x.hashCode(), y.hashCode());
+    }
+
+    @Test
+    public void testHashCode_notEqualsObjects() throws Exception {
+        assertNotEquals(x.hashCode(), new EntityConfigurationField("id").hashCode());
+    }
+
+}
